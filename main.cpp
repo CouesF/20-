0322efa990eea7b840dbc717d5c0eb89149a9b38
CouesF <fangyuyang2002@outlook.com>
2020-12-01 15:01:33 +0800

    circleDetect

--- main.cpp
+++ main.cpp
@@ -39,111 +39,54 @@ Create Topic: RobotPositionInfo
 #define areaXCount imgWidth/imgPartitionSize+3
 #define areaYCount imgWidth/imgPartitionSize+3
 #define gaussianSumMax 800//used for debug draw on canvas
+int cutX1 = 10,
+    cutY1 = 10, 
+    cutX2 = 300, 
+    cutY2 = 300;
+
+Rect imgResize(cutX1,cutY1,cutX2,cutY2);//TODO: 
 
 using namespace cv;
 
-//TODO IMPORTANT : 100pixels = 30CM (height 60cm, Direction 30 degrees)
+//TODO: IMPORTANT : 100pixels = 30CM (height 60cm, Direction 30 degrees)
 
-Point2f perspectiveTransformOriginPoint[4] = {Point2f(155,0),Point2f(485,0),Point2f(0,320),Point2f(640,320)};
-Point2f perspectiveTransformWarpedPointa[4] = {Point2f(0,0),Point2f(warpedWidth,0),Point2f(0,warpedHeight),Point2f(warpedWidth,warpedHeight)};
-int xGridLinesFitting[int(maxLensInImg *2 + 20)];
-int yGridLinesFitting[int(maxLensInImg *2 + 20)];
-float gaussianPara[int(pixelsCntPerCentimeter*3+2)];
-void calculateGaussianPara()
+
+Point circleCentralPointDetection(Mat img)
 {
-    double para = 1.0/sqrt(2.0*CV_PI);
-    double gap = 4.0 / pixelsCntPerCentimeter / 3.0;
-    for(int i = 1;i <= pixelsCntPerCentimeter * 3;i++)
+    Mat croppedImg = img(imgResize);
+    Mat grayImg,blurredImg,mThres_Gray;
+    cvtColor(croppedImg,grayImg,COLOR_RGB2GRAY);
+    medianBlur(grayImg,blurredImg,3);
+    std::vector<Vec3f>circles;
+    cannyMaxThreshold = min(200,(int)(threshold(blurredImg,mThres_Gray,0,255,THRESH_OTSU)));
+    HoughCircles(blurredImg,circles,HOUGH_GRADIENT,1,0,cannyMaxThreshold,80,120)//TODO: the last 2 paras is the min & max radius of circle
+    Mat centerCanvas(cutX2 - cutX1 , cutY2 - cutY1,CV_8UC1,Scalar(0));
+    for(size_t i = 0; i < circles.size(); i++)
     {
-        gaussianPara[i] = para * exp(-pow((double)(-2+(double)i*gap),2)/2.0)*30.0;
+        Vec3i c = circles[i];
+        Point center = Point(c[0],c[1]);
+        uchar *rowData = centerCanvas.ptr<uchar>(c[0]);
+        rowData[c[1]] = Scalar(255);
+        //int x = circles[i]
     }
-}
+    imshow("centerCanvas",centerCanvas);
+    GaussianBlur(centerCanvas,centerCanvas,Size(5,5),0);
+    imshow("blur",centerCanvas);
+    double minVal;
+    double maxVal;
+    Point minIdx;
+    Point maxIdx;
+    minMaxLoc(centerCanvas,&minVal, &maxVal, &minIdx, &maxIdx);
+    circle(croppedImg,maxIdx,3,(0,0,255),0)
+    imshow("circleDetect",croppedImg);
+    waitKey(0);
 
-
-void gaussianSum(int pixelPosition,int k)// TODO:rewrite using array
-{
-    pixelPosition += maxLensInImg;
-    int pixelTempPos;
-    for(int i = 1;i <= pixelsCntPerCentimeter * 3 ;i++)
-    {
-        pixelTempPos = int(pixelPosition + i - pixelsCntPerCentimeter * 3.0 / 2.0 );
-        //if(pixelTempPos > maxLensInImg || pixelTempPos < 1) continue;
-        if(k == 0)
-        {
-            xGridLinesFitting[pixelTempPos] += double(gaussianPara[i]);
-        }
-        if(k == 1)
-        {
-            yGridLinesFitting[pixelTempPos] += double(gaussianPara[i]);
-        }
-    }
-}
-
-float getAngelOfTwoVector(Point2f &pt1, Point2f &pt2, Point2f &c)
-{
-	float theta = atan2(pt1.x - c.x, pt1.y - c.y) - atan2(pt2.x - c.x, pt2.y - c.y);
-	if (theta > CV_PI)
-		theta -= 2 * CV_PI;
-	if (theta < -CV_PI)
-		theta += 2 * CV_PI;
- 
-	//theta = theta * 180.0 / CV_PI;
-	return theta;
 }
 
 
 int main(int argc, char **argv)
 {
-    //data type initialize
-        //TODO:Mat cameraMatrix = (Mat1d(3,3) << fx, 0, cx, fy, cy, 0, 0, 1);
-        //Mat distortionCoefficients = (Mat1d(1,4) << k1, k2, p1, p2);
-    Mat img,originImg;
-    Mat warpedImg,warpedGrayImg;
-
-    short thresholdCnt = 151 ;//used for estimating the threshold value of canny edge detection. to save the resources of calculating while looping
-    int cannyMinThreshold,cannyMaxThreshold;
     VideoCapture cap;
-    
-    //Localization data
-        //    The first parameter represents the frame order. 0-previous 1-current
-        //    The last parameter represents x or y values
-        //bool isCrossExists[2][areaXCount][areaYCount];
-        //int localizationData[2][areaXCount][areaYCount][2];
-        //int coord[2][areaXCount][areaYCount][2];
-    double xDirectionOfPreviousFrame = 0,yDirectionOfPreviousFrame = CV_PI/2;//theta in img.
-    double robotGlobalDirection =  -CV_PI / 2; //robot direction in global map. Initial direction is -pi/2
-    double robotGlobalX = 0,robotGlobalY = 0;
-    int xGlobal = 0, yGlobal = 0;
-
-    int xRho,yRho,xPreviousRho = 0,yPreviousRho = 0;
-
-    //TODO: initial the point
-    int previousCrossExists[8][8];//6*6means devide the img into 6*6 cells
-    memset(previousCrossExists,0,sizeof(previousCrossExists));
-    double previousCrossPosition[8][8][2];//0-x 1-y
-    previousCrossExists[1][1] = 1;
-    previousCrossExists[2][1] = 1;
-    previousCrossExists[4][1] = 1;
-    previousCrossExists[1][2] = 1;
-    previousCrossExists[1][4] = 1;
-    previousCrossExists[2][2] = 1;
-    previousCrossExists[4][4] = 1;
-    previousCrossPosition[1][1][0] = 10;
-    previousCrossPosition[1][1][1] = 10;
-    previousCrossPosition[2][1][0] = 110;
-    previousCrossPosition[2][1][1] = 10;
-    previousCrossPosition[4][1][0] = 210;
-    previousCrossPosition[4][1][1] = 10;
-    previousCrossPosition[1][2][0] = 10;
-    previousCrossPosition[1][2][1] = 110;
-    previousCrossPosition[1][4][0] = 10;
-    previousCrossPosition[1][4][1] = 210;
-    previousCrossPosition[2][2][0] = 110;
-    previousCrossPosition[2][2][1] = 110;
-    previousCrossPosition[4][4][0] = 210;
-    previousCrossPosition[4][4][1] = 210;
-    
-
 
     // cap initialization and setting
     cap.open(0);
@@ -189,3 +132,8 @@ int main(int argc, char **argv)
             cap.grab();
         //while(cap.grab()){}
         cap >> originImg;
+
+
+    }
+
+}
